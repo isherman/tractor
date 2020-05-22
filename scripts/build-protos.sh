15d44d6baa01b3e448e07eb87015efb120be8544
Ian Sherman <ian.j.sherman@gmail.com>
2020-05-21 20:51:22 -0700

    Clean up proto compilation

--- scripts/build-protos.sh
+++ scripts/build-protos.sh
@@ -4,11 +4,10 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CMD="protoc"
 CMD_ARGS="--proto_path=protos
-                 --python_out=python/genproto
-                 --twirp_swagger_out=app/genswagger
-                 --twirp_python_srv_out=python/gensrv
-                 protos/farmng/tractor/v1/*.proto"
-               # protos/validate/validate.proto"
+          --python_out=python/genproto
+          --twirp_tornado_srv_out=python/gensrv
+          protos/farmng/tractor/v1/*.proto"
+          # protos/validate/validate.proto"
 TAG="protoc"
 
 docker build -t $TAG -f $DIR/Dockerfile.protoc $DIR
