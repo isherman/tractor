d1197e9b8e4e3e09baa05edd5abed109009dfe2b
Ian <ian.j.sherman@gmail.com>
2020-09-14 20:45:27 -0400

    Full log

--- scripts/collect-logs.sh
+++ scripts/collect-logs.sh
@@ -5,7 +5,7 @@ LOGDIR=`mktemp -d -t farm-ng-log-$(date +"%s")-XXX`
 systemctl list-unit-files | grep "tractor" | awk '{print $1}' | while IFS= read -r unit; do
   logfile=$LOGDIR/$unit.log
   echo "Writing to $logfile"
-  journalctl -b --output=short-iso-precise --no-pager -u $unit | head -1000 > $logfile
+  journalctl -b --output=short-iso-precise --no-pager -u $unit > $logfile
 done
 
 logfile=$LOGDIR/systemd.log
