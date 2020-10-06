30deb3bf493fe87eafeeade35c0e70ac6cbe7430
Ethan Rublee <ethan.rublee@gmail.com>
2020-10-06 02:31:39 -0700

    Simplify support for holding const.

--- cpp/farm_ng/calibration/local_parameterization.cpp
+++ cpp/farm_ng/calibration/local_parameterization.cpp
@@ -42,7 +42,7 @@ LocalParameterizationSE3::~LocalParameterizationSE3() {}
 
 namespace {
 struct SE3Plus {
-  SE3Plus(std::array<int, 6> constancy_mask)
+  SE3Plus(const std::array<int, 6>& constancy_mask)
       : constancy_mask_(constancy_mask) {}
 
   template <typename Scalar>
@@ -61,7 +61,7 @@ struct SE3Plus {
     T_plus_delta = T * Sophus::SE3<Scalar>::exp(delta);
     return true;
   }
-  std::array<int, 6> constancy_mask_;
+  const std::array<int, 6>& constancy_mask_;
 };
 
 // Utility class for computing Dx_this_mul_exp_x_at_0 using
@@ -75,7 +75,7 @@ struct SE3PlusAt0 {
     return SE3Plus(constancy_mask_)(T.data(), delta_raw, T_plus_delta_raw);
   }
   SE3d T_;
-  std::array<int, 6> constancy_mask_;
+  const std::array<int, 6>& constancy_mask_;
 };
 }  // namespace
 
@@ -91,20 +91,11 @@ bool LocalParameterizationSE3::Plus(double const* T_raw,
 //
 bool LocalParameterizationSE3::ComputeJacobian(double const* T_raw,
                                                double* jacobian_raw) const {
-  if (LocalSize() == 0) {
-    return true;
-  }
-
-#define _USE_CERES_AUTODIFF_SE3
-#ifdef _USE_CERES_AUTODIFF_SE3
-  // Comparing using ceres autodiff versus the generated code in Sophus.
+  // Compared using ceres autodiff versus the generated code in Sophus.
   // This is as fast or faster, and seems to be slight bit more accurate (but
   // really down in the precision bit flipping). This allows for derivative of
   // exp(x)*T or T*exp(x)
   using ParameterDims = typename ceres::SizedCostFunction<7, 6>::ParameterDims;
-  Eigen::Matrix<double, 7, 6, Eigen::RowMajor> se3_jacobian;
-  double* se3_jacobian_raw = se3_jacobian.data();
-
   std::array<double, 6> zero({0.0, 0.0, 0.0, 0.0, 0.0, 0.0});
   std::array<const double*, 1> parameters({zero.data()});
   constexpr int kOutput = 7;
@@ -112,34 +103,7 @@ bool LocalParameterizationSE3::ComputeJacobian(double const* T_raw,
 
   ceres::internal::AutoDifferentiate<kOutput, ParameterDims>(
       SE3PlusAt0(Eigen::Map<SE3d const>(T_raw), constancy_mask_),
-      parameters.data(), kOutput, output, &se3_jacobian_raw);
-#else
-  Eigen::Map<SE3d const> T(T_raw);
-  Eigen::Matrix<double, 7, 6, Eigen::RowMajor> se3_jacobian =
-      T.Dx_this_mul_exp_x_at_0();
-#endif
-  Eigen::Map<Eigen::Matrix<double, 7, Eigen::Dynamic, Eigen::RowMajor>>
-      jacobian(jacobian_raw, 7, LocalSize());
-
-  // no values held constant, so just use the se3 jacobian
-  if (LocalSize() == 6) {
-    jacobian = se3_jacobian;
-    return true;
-  }
-
-  // Compute the jacobian for the variables held constant.
-  // See ceres::SubsetParameterization for inspiration.
-  Eigen::Matrix<double, 6, Eigen::Dynamic, Eigen::RowMajor> const_jacobian(
-      6, LocalSize());
-  const_jacobian.setZero();
-  for (int i = 0, j = 0; i < 6; ++i) {
-    if (!constancy_mask_[i]) {
-      const_jacobian(i, j++) = 1.0;
-    }
-  }
-  // TODO(ethanrublee) Confirm this is correct, they seem to compose and the
-  // dimensions line up. Effectively the constancy_mask is eliminating rows
-  jacobian = se3_jacobian * const_jacobian;
+      parameters.data(), kOutput, output, &jacobian_raw);
   return true;
 }
 
