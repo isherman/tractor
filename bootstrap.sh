cf0e6b227f3ea6ad39a5bd3cb97350a1c3fe343c
Ian Sherman <ian.j.sherman@gmail.com>
2020-10-27 00:22:02 -0700

    Cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,5 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 export FARM_NG_ROOT=$( cd "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )
 
-# $FARM_NG_ROOT/bootstrap-apt.sh
-$FARM_NG_ROOT/bootstrap-base.sh
+$FARM_NG_ROOT/bootstrap-apt.sh
 $FARM_NG_ROOT/bootstrap-venv.sh
