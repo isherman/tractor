801e881103b358ae773b31897fac6d456ae744a6
Ian Sherman <ian.j.sherman@gmail.com>
2020-12-08 22:08:04 -0800

    Iterate on calibrate_robot_extrinsics

--- modules/calibration/cpp/farm_ng/calibrate_robot_extrinsics.cpp
+++ modules/calibration/cpp/farm_ng/calibrate_robot_extrinsics.cpp
@@ -8,8 +8,8 @@
 #include <grpcpp/create_channel.h>
 #include <grpcpp/security/credentials.h>
 
+#include "farm_ng/calibration/calibrate_robot_extrinsics.pb.h"
 #include "farm_ng/calibration/robot_hal.grpc.pb.h"
-#include "farm_ng/calibration/workspace.pb.h"
 #include "farm_ng/core/blobstore.h"
 
 using farm_ng::core::ReadProtobufFromJsonFile;
@@ -35,7 +35,8 @@ class RobotHalClient {
     // Block waiting for response
     CapturePoseResponse response;
     stream->Read(&response);
-    std::cout << "Got response " << response.ShortDebugString() << std::endl;
+    std::cout << "Got response " << response.ShortDebugString() << "\n"
+              << std::endl;
 
     return {stream->Finish(), response};
   }
@@ -44,12 +45,12 @@ class RobotHalClient {
   std::unique_ptr<RobotHALService::Stub> stub_;
 };
 
-std::vector<CapturePoseRequest> CapturePoseRequestsFromConfiguration(
-    SampledCartesianWorkspace config) {
+std::vector<CapturePoseRequest> CapturePoseRequestsFromSampledWorkspace(
+    SampledCartesianWorkspace sampled_workspace) {
   std::vector<CapturePoseRequest> pose_requests;
-  int sample_count_x = std::max(config.sample_count_x(), 1);
-  int sample_count_y = std::max(config.sample_count_y(), 1);
-  int sample_count_z = std::max(config.sample_count_z(), 1);
+  int sample_count_x = std::max(sampled_workspace.sample_count_x(), 1);
+  int sample_count_y = std::max(sampled_workspace.sample_count_y(), 1);
+  int sample_count_z = std::max(sampled_workspace.sample_count_z(), 1);
 
   auto coordinate = [=](int i, double size, int sample_count) -> double {
     if (i == 0) {
@@ -66,12 +67,12 @@ std::vector<CapturePoseRequest> CapturePoseRequestsFromConfiguration(
       for (double iz = 0; iz < sample_count_z; iz++) {
         CapturePoseRequest pose_request;
         NamedSE3Pose* pose = pose_request.add_poses();
-        pose->mutable_a_pose_b()->mutable_position()->set_x(
-            coordinate(ix, config.workspace().size().x(), sample_count_x));
-        pose->mutable_a_pose_b()->mutable_position()->set_y(
-            coordinate(iy, config.workspace().size().y(), sample_count_y));
-        pose->mutable_a_pose_b()->mutable_position()->set_z(
-            coordinate(iz, config.workspace().size().z(), sample_count_z));
+        pose->mutable_a_pose_b()->mutable_position()->set_x(coordinate(
+            ix, sampled_workspace.workspace().size().x(), sample_count_x));
+        pose->mutable_a_pose_b()->mutable_position()->set_y(coordinate(
+            iy, sampled_workspace.workspace().size().y(), sample_count_y));
+        pose->mutable_a_pose_b()->mutable_position()->set_z(coordinate(
+            iz, sampled_workspace.workspace().size().z(), sample_count_z));
         // TODO: add to workspace.origin
         // TODO: rotation
         // SophusToProto(se3, "workspace_origin", "target", pose)
@@ -84,38 +85,48 @@ std::vector<CapturePoseRequest> CapturePoseRequestsFromConfiguration(
 
 class CalibrateRobotExtrinsicsProgram {
  public:
-  CalibrateRobotExtrinsicsProgram(std::shared_ptr<RobotHalClient> client)
-      : client_(client) {}
+  CalibrateRobotExtrinsicsProgram(
+      std::shared_ptr<RobotHalClient> client,
+      const CalibrateRobotExtrinsicsConfiguration& configuration)
+      : client_(client), configuration_(configuration) {}
 
   void Run() {
-    auto config =
-        ReadProtobufFromJsonFile<SampledCartesianWorkspace>("workspace.json");
+    auto requests = CapturePoseRequestsFromSampledWorkspace(
+        configuration_.sampled_workspace());
 
-    auto capture_pose_requests = CapturePoseRequestsFromConfiguration(config);
-
-    for (auto& pose : capture_pose_requests) {
-      auto [status, response] = client_->CapturePoseSync(pose);
+    for (auto& request : requests) {
+      auto [status, response] = client_->CapturePoseSync(request);
 
       // TODO(isherman): Pipe this into calibration
       CHECK(status.ok());
       CHECK_EQ(response.status(), CapturePoseResponse::STATUS_SUCCESS);
-      CHECK_GT(response.image_rgb().resource().bytes().length(), 0);
+      CHECK_GT(response.image_rgb().resource().data().length(), 0);
       CHECK_GT(response.image_rgb().camera_model().image_width(), 0);
     }
   }
 
  private:
   std::shared_ptr<RobotHalClient> client_;
+  CalibrateRobotExtrinsicsConfiguration configuration_;
 };
 
 }  // namespace farm_ng::calibration
 
 int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    std::cerr << "Must pass absolute path to configuration" << std::endl;
+    return 1;
+  }
+  std::string configuration_path = argv[1];
   std::string server_address = "localhost:50051";
   auto credentials = grpc::InsecureChannelCredentials();
   auto channel = grpc::CreateChannel(server_address, credentials);
   auto client = std::make_shared<farm_ng::calibration::RobotHalClient>(channel);
-  auto program = farm_ng::calibration::CalibrateRobotExtrinsicsProgram(client);
+  auto configuration = ReadProtobufFromJsonFile<
+      farm_ng::calibration::CalibrateRobotExtrinsicsConfiguration>(
+      configuration_path);
+  auto program = farm_ng::calibration::CalibrateRobotExtrinsicsProgram(
+      client, configuration);
   program.Run();
 
   return 0;
