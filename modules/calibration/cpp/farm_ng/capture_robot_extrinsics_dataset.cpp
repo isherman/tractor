6e9cf9ec2b6f9fa0fac2c58c9b5511579f52ecb3
Ethan Rublee <ethan.rublee@gmail.com>
2020-12-09 15:53:11 -0800

    Make capture_robot_extrinsic_dataset.cpp conform to program concept. Save log data directly (does not use ipc logger).

--- modules/calibration/cpp/farm_ng/capture_robot_extrinsics_dataset.cpp
+++ modules/calibration/cpp/farm_ng/capture_robot_extrinsics_dataset.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 
+#include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <grpc/grpc.h>
 #include <grpcpp/channel.h>
@@ -11,12 +12,23 @@
 #include "farm_ng/calibration/capture_robot_extrinsics_dataset.pb.h"
 #include "farm_ng/calibration/robot_hal.grpc.pb.h"
 #include "farm_ng/core/blobstore.h"
+#include "farm_ng/core/event_log.h"
+#include "farm_ng/core/init.h"
+#include "farm_ng/core/ipc.h"
+
 #include "farm_ng/perception/sophus_protobuf.h"
 
-using farm_ng::core::ReadProtobufFromJsonFile;
-using farm_ng::perception::NamedSE3Pose;
-using farm_ng::perception::Image;
+DEFINE_bool(interactive, false, "receive program args via eventbus");
 
+// TODO should this be a resource path, relative to blobstore?
+DEFINE_string(configuration_path, "/host/config.json",
+              "Path to the configuration file.");
+
+using farm_ng::core::MakeEvent;
+using farm_ng::core::ReadProtobufFromJsonFile;
+using farm_ng::perception::Image;
+using farm_ng::perception::NamedSE3Pose;
+typedef farm_ng::core::Event EventPb;
 
 namespace farm_ng::calibration {
 class RobotHalClient {
@@ -69,8 +81,9 @@ std::vector<CapturePoseRequest> CapturePoseRequestsFromSampledWorkspace(
     for (double iy = 0; iy < sample_count_y; iy++) {
       for (double iz = 0; iz < sample_count_z; iz++) {
         Sophus::SE3d base_pose_workspace;
-        ProtoToSophus(sampled_workspace.workspace().base_pose_workspace().a_pose_b(),
-                      &base_pose_workspace);
+        ProtoToSophus(
+            sampled_workspace.workspace().base_pose_workspace().a_pose_b(),
+            &base_pose_workspace);
         Sophus::SE3d workspace_pose_target = Sophus::SE3d::trans(
             target_coordinate(ix, sampled_workspace.workspace().size().x(),
                               sample_count_x),
@@ -93,51 +106,128 @@ std::vector<CapturePoseRequest> CapturePoseRequestsFromSampledWorkspace(
 class CaptureRobotExtrinsicsDatasetProgram {
  public:
   CaptureRobotExtrinsicsDatasetProgram(
-      std::shared_ptr<RobotHalClient> client,
+      core::EventBus& bus,
       const CaptureRobotExtrinsicsDatasetConfiguration& configuration)
-      : client_(client), configuration_(configuration) {}
+      : bus_(bus),
+        timer_(bus_.get_io_service()),
+        configuration_(configuration) {
+    if (FLAGS_interactive) {
+      status_.mutable_input_required_configuration()->CopyFrom(configuration);
+    } else {
+      set_configuration(configuration);
+    }
+    bus_.AddSubscriptions({bus_.GetName()});
 
-  void Run() {
+    bus_.GetEventSignal()->connect(
+        std::bind(&CaptureRobotExtrinsicsDatasetProgram::on_event, this,
+                  std::placeholders::_1));
+    on_timer(boost::system::error_code());
+  }
+  void send_status() {
+    bus_.Send(MakeEvent(bus_.GetName() + "/status", status_));
+  }
+
+  void on_timer(const boost::system::error_code& error) {
+    if (error) {
+      LOG(WARNING) << "timer error: " << __PRETTY_FUNCTION__ << error;
+      return;
+    }
+    timer_.expires_from_now(boost::posix_time::millisec(1000));
+    timer_.async_wait(std::bind(&CaptureRobotExtrinsicsDatasetProgram::on_timer,
+                                this, std::placeholders::_1));
+
+    send_status();
+  }
+
+  bool on_configuration(const EventPb& event) {
+    CaptureRobotExtrinsicsDatasetConfiguration configuration;
+    if (!event.data().UnpackTo(&configuration)) {
+      return false;
+    }
+    LOG(INFO) << configuration.ShortDebugString();
+    set_configuration(configuration);
+    return true;
+  }
+
+  void set_configuration(
+      const CaptureRobotExtrinsicsDatasetConfiguration& configuration) {
+    configuration_ = configuration;
+    status_.clear_input_required_configuration();
+    send_status();
+  }
+
+  void on_event(const EventPb& event) {
+    if (on_configuration(event)) {
+      return;
+    }
+  }
+
+  int run() {
+    while (status_.has_input_required_configuration()) {
+      bus_.get_io_service().run_one();
+    }
+
+    WaitForServices(bus_, {});
+
+    auto credentials = grpc::InsecureChannelCredentials();
+    auto channel =
+        grpc::CreateChannel(configuration_.hal_service_address(), credentials);
+    farm_ng::calibration::RobotHalClient client(channel);
+
+    std::string log_path = (core::GetBucketRelativePath(core::BUCKET_LOGS) /
+                            boost::filesystem::path(configuration_.name()))
+                               .string();
+
+    core::SetArchivePath(log_path);
+
+    auto resource_path =
+        farm_ng::core::GetUniqueArchiveResource("events", "log", "bi");
+
+    core::EventLogWriter log_writer(resource_path.second);
+
     auto requests = CapturePoseRequestsFromSampledWorkspace(
         configuration_.sampled_workspace());
 
     for (auto& request : requests) {
-      auto [status, response] = client_->CapturePoseSync(request);
+      bus_.get_io_service().poll();
+
+      log_writer.Write(core::MakeEvent("capture/request", request));
+      auto [status, response] = client.CapturePoseSync(request);
+      log_writer.Write(core::MakeEvent("capture/response", response));
 
       // TODO(isherman): Pipe this into calibration
       CHECK(status.ok());
       CHECK_EQ(response.status(), CapturePoseResponse::STATUS_SUCCESS);
-      for(const Image& image: response.images()) {
+      for (const Image& image : response.images()) {
         CHECK_GT(image.resource().data().length(), 0);
         CHECK_GT(image.camera_model().image_width(), 0);
       }
-
     }
+    return 0;
   }
 
  private:
-  std::shared_ptr<RobotHalClient> client_;
+  core::EventBus& bus_;
+  boost::asio::deadline_timer timer_;
+
   CaptureRobotExtrinsicsDatasetConfiguration configuration_;
+  CaptureRobotExtrinsicsDatasetStatus status_;
 };
 
 }  // namespace farm_ng::calibration
 
-int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    std::cerr << "Must pass absolute path to configuration" << std::endl;
-    return 1;
-  }
-  std::string configuration_path = argv[1];
-  std::string server_address = "localhost:50051";
-  auto credentials = grpc::InsecureChannelCredentials();
-  auto channel = grpc::CreateChannel(server_address, credentials);
-  auto client = std::make_shared<farm_ng::calibration::RobotHalClient>(channel);
+int Main(farm_ng::core::EventBus& bus) {
   auto configuration = ReadProtobufFromJsonFile<
       farm_ng::calibration::CaptureRobotExtrinsicsDatasetConfiguration>(
-      configuration_path);
-  auto program = farm_ng::calibration::CaptureRobotExtrinsicsDatasetProgram(
-      client, configuration);
-  program.Run();
+      FLAGS_configuration_path);
 
-  return 0;
+  farm_ng::calibration::CaptureRobotExtrinsicsDatasetProgram program(
+      bus, configuration);
+  return program.run();
+}
+
+void Cleanup(farm_ng::core::EventBus& bus) { LOG(INFO) << "Cleanup"; }
+
+int main(int argc, char* argv[]) {
+  return farm_ng::core::Main(argc, argv, &Main, &Cleanup);
 }
