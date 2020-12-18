7d2ede3062b755b4d929f14aba4ea4e54e1dd5bb
Ian Sherman <ian.j.sherman@gmail.com>
2020-12-18 11:10:41 -0800

    Cleanup

--- modules/perception/tests/integration_test.py
+++ modules/perception/tests/integration_test.py
@@ -1,14 +1,8 @@
-# import subprocess
 import unittest
 
 
 class TestCase(unittest.TestCase):
-
-    # def test_io(self):
-    #     exitcode = subprocess.call("docker-compose up --build --abort-on-container-exit", shell=True)
-    #     self.assertEqual(exitcode, 0)
-
-    def test_resource(self):
+    def test_foo(self):
         self.assertTrue(True)
 
 
