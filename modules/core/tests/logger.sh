502c855c63a44bd5e61361c25e86c3ada5117198
Ian Sherman <ian.j.sherman@gmail.com>
2020-12-19 15:16:59 -0800

    Add docker-compose pull

--- modules/core/tests/logger.sh
+++ modules/core/tests/logger.sh
@@ -3,10 +3,9 @@ TMPDIR=$(mktemp -d)
 OVERRIDE="docker-compose.ipc_publisher_test.yml"
 SERVICE_DEPS="ipc_logger"
 
-_UID=`id -u` _GID=`id -g` BLOBSTORE_ROOT=$TMPDIR \
-docker-compose \
--f docker-compose.yml \
--f $OVERRIDE \
-up --abort-on-container-exit $SERVICE_DEPS test_runner
+docker-compose pull
+
+_UID=`id -u` _GID=`id -g` BLOBSTORE_ROOT=$TMPDIR docker-compose -f docker-compose.yml \
+-f $OVERRIDE up --abort-on-container-exit $SERVICE_DEPS test_runner
 
 rm -rf $TMPDIR
