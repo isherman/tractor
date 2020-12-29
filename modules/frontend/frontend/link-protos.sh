bbdac262032e3215d9a1c2e75a11106ed0d11515
Ian Sherman <ian.j.sherman@gmail.com>
2020-12-28 16:26:24 -0800

    Reduce boilerplate

--- modules/frontend/frontend/link-protos.sh
+++ modules/frontend/frontend/link-protos.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for module in core calibration frontend perception tractor
+for dir in $FARM_NG_ROOT/modules/*
 do
+  # Module name from path (https://stackoverflow.com/questions/9011233)
+  module="${dir##*/}"
   cd $FARM_NG_ROOT/build/modules/$module/protos/ts && yarn
 done
